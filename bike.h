4f03da4f591c2c3898aabba820b2342f92552d7e
Spoofsters <rafaelsebmar@gmail.com>
2025-08-07 11:18:42 -0400

    Added Header, Timer Functionality & main.c setup

---
+++ bike.h
@@ -0,0 +1,16 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <stdint.h>
+#include <util/twi.h>
+#include <cys.clk.h>
+
+uint8_t ONGOING_TIMER;
+uint8_t CURRENT_TIMER;
+uint8_t START_TIMER;
+
+void TIMER_0(); //Use Timer0
+void millis(); //timer function in milliseconds
